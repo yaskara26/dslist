b260a07e87372ebedecede8bc174d02c56d406b8
Yaskara Santangelo <yaskaraguimaraes@hotmail.com>
2025-01-24 18:43:59 -0300

    Homolog ok

--- src/main/java/com/yaskarasantangelo/dslist/projections/GameMinProjection.java
+++ src/main/java/com/yaskarasantangelo/dslist/projections/GameMinProjection.java
@@ -3,7 +3,7 @@ package com.yaskarasantangelo.dslist.projections;
 public interface GameMinProjection {
 	Long getId();
 	String getTitle();
-	Integer getYear();
+	Integer getGameYear();
 	String getImgUrl();
 	String getShortDescription();
 	Integer getPosition();
